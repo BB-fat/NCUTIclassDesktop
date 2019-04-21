c74b825ecc55ca6c518aeb950743bde8a575aed6
BB-fat <1056871944@qq.com>
2019-04-21 15:08:20 +0800

    release 1.2

---
+++ release/Max OS/iclassDesktop1.2.app/Contents/Resources/main.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from myApp import myAPP
+
+if __name__=="__main__":
+    app=QApplication(sys.argv)
+    myWidget=myAPP()
+    myWidget.show()
+    sys.exit(app.exec_())
